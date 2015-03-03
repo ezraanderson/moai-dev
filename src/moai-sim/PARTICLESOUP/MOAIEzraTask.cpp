6ac216a6e51a7539f9c2d581357e67ed0cce0fdf
ezraanderson <ezraanderson1979@yahoo.com>
2015-03-02 23:46:27 -0500

    particle soup moved files to folder

---
+++ src/moai-sim/PARTICLESOUP/MOAIEzraTask.cpp
@@ -0,0 +1,169 @@
+// Copyright (c) 2010-2011 Zipline Games, Inc. All Rights Reserved.
+// http://getmoai.com
+
+#include "pch.h"
+#include <moai-sim/MOAIBox.h>
+#include <moai-sim/MOAIEzraTask.h>
+#include <moai-sim/MOAISim.h>
+
+//================================================================//
+// MOAIEzraTask
+//================================================================//
+
+
+void MOAIEzraTask::capFrameRate(double fps) {
+
+	
+    wait = 1 / fps;
+    diff = ZLDeviceTime::GetTimeInSeconds () - start;
+
+    if (diff < wait) {
+   
+		//#ifdef MOAI_OS_ANDROID 
+				//usleep(wait - diff);
+				//usleep((wait - diff)*1000000);
+				// __android_log_print(ANDROID_LOG_INFO, "C++", "sleep %s", "sim");
+		//#endif
+
+		//#ifndef MOAI_OS_WINDOWS
+		//		Sleep ( wait - diff );
+		//		printf(">> sleep %f  %f %f %f \n",wait , diff, USDeviceTime::GetTimeInSeconds (),wait - diff );
+		//#endif 
+
+
+
+    }
+    start = ZLDeviceTime::GetTimeInSeconds ();
+}
+       
+
+//----------------------------------------------------------------//
+void MOAIEzraTask::Execute () {
+
+
+
+
+
+   //while (this->mLoopState) {
+   //     this->mMutex.Lock();
+   //     // printf("LOOPING  THREAD\n");
+   //      this->mMutex.Unlock();
+   // };
+
+
+  // this->mMutex.Clear();
+ // this->mthr
+
+    //if ( this->mLoopState == PAUSED ) {
+	//	return;
+	//}
+
+    //while (this->mLoopState) {
+    //     printf("LOOPING  THREAD\n");
+    //};
+	
+	while (this->mLoopState) {
+
+       //if (this->mLoopState) { return 1; };
+	   //printf("C>>>>>>>>>>>>>THREAD--Execute\n");
+        //
+			//this->mMutex.Lock();	
+			   this->mData->Loop();				
+			//this->mMutex.Unlock();
+
+			//capFrameRate(60);
+
+
+           // printf("LOOPING  THREAD\n");
+
+               //#ifndef MOAI_OS_WINDOWS
+              //      usleep(1000);
+              // #endif
+
+	};
+
+
+
+      printf("\n **** 2. MOAIEzraTask -> SHUT DOWN THREAD DONE **** \n");
+
+
+};
+
+
+
+void MOAIEzraTask::threadRunning ( bool isRun ) {
+
+	//this->mMutex.Lock();
+         this->mLoopState = isRun;
+         printf("----- mLoopState\n");
+
+	//this->mMutex.Unlock();
+
+};
+
+
+//void MOAIEzraTask::threadStop ( bool isRun ) {
+//    this->mLoopState = isRun;
+//};
+
+
+
+
+//----------------------------------------------------------------//
+void MOAIEzraTask::Init ( MOAIBox& target, u32 action ) {
+
+	///printf("C>>>>>>>>>>>>>THREAD--INIT\n");
+	//this->mPriority = PRIORITY_LOW;
+    this->mLoopState = true;
+	this->mData.Set ( *this, &target );
+	
+
+	//this->mAction = action;
+
+
+};
+
+
+
+//----------------------------------------------------------------//
+MOAIEzraTask::MOAIEzraTask () :
+	mAction      ( NONE ),
+    mLoopState   ( false )
+	{	
+	RTTI_SINGLE ( MOAITask )
+}
+
+
+//
+////----------------------------------------------------------------//
+MOAIEzraTask::~MOAIEzraTask () {   
+        // printf("a \n");
+      
+        this->mData.Set ( *this, 0 ); 
+        this->mLoopState =   false;
+  printf("\n \n **** 1. MOAIEzraTask --> FIRE DESTRUCTOR  ****  \n\n");
+
+        // printf("b \n");
+}
+
+
+//----------------------------------------------------------------//
+void MOAIEzraTask::Publish () {
+
+
+
+
+	//if ( this->mInflateOnLoad && ( !this->mInflateOnTaskThread )) {
+	//	this->mData->Inflate ( this->mWindowBits );
+	//}
+
+	//if ( this->mOnFinish ) {
+	
+			//	MOAILuaStateHandle state = MOAILuaRuntime::Get ().State ();
+			//	this->PushLocal ( state, this->mOnFinish );
+			//	this->mData->PushLuaUserdata ( state );
+			//	state.DebugCall ( 1, 0 );
+	//}
+}
+
+
---
+++ src/moai-sim/PARTICLESOUP/MOAIEzraTask.h
@@ -0,0 +1,67 @@
+// Copyright (c) 2010-2011 Zipline Games, Inc. All Rights Reserved.
+// http://getmoai.com
+
+#ifndef MOAIEZRATHREAD_H
+#define MOAIEZRATHREAD_H
+
+#include <moai-util/MOAITask.h>
+
+class MOAIDataBuffer;
+
+//================================================================//
+// MOAIEzraTask
+//================================================================//
+class MOAIEzraTask : 
+	public MOAITask {
+private:
+
+	STLString								mFilename;
+	MOAILuaSharedPtr < MOAIBox >			mData;
+	MOAILuaMemberRef						mOnFinish;
+	u32										mAction;
+	MOAIMutex								mMutex;
+    bool									mLoopState;
+	//bool									mInflateOnLoad;
+	//bool									mInflateOnTaskThread;
+	//int									mWindowBits;
+
+	//THROTTLE
+	double wait;  
+	double diff;  
+	double start;  
+
+	//----------------------------------------------------------------//
+
+
+public:
+
+	enum {
+		NONE,
+		LOAD_ACTION,
+		SAVE_ACTION,
+	};
+
+
+
+	DECL_LUA_FACTORY ( MOAIEzraTask )
+
+	//----------------------------------------------------------------//
+	void		Init				( MOAIBox& target, u32 action );
+				MOAIEzraTask		();
+				~MOAIEzraTask		();
+
+	void	    Execute				();
+    void	    threadRunning		(bool isRun);
+	void		Publish				();
+	void		capFrameRate		(double fps);
+
+
+
+	//void		RegisterLuaClass	( MOAILuaState& state );
+	//void		RegisterLuaFuncs	( MOAILuaState& state );
+	//void		SaveData			( cc8* filename, MOAIDataBuffer& target );
+	//void		SetCallback			( lua_State* L, int idx );
+	//void		SetInflateOnLoad	( bool inflateOnLoad, bool inflateOnTaskThread, int windowBits );
+};
+
+#endif
